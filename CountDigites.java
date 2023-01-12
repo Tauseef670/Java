c8e5ba0798b25fd5b6b0d8b43df91067ea226fff
Tauseef Jamal <121728120+Tauseef670@users.noreply.github.com>
2023-01-12 16:20:13 +0530

    Add files via upload

---
+++ CountDigites.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountDigites {
+    public static void main(String[] args) {
+        
+        int n , count =0;
+        System.out.println("Enter any number");
+
+        Scanner r = new Scanner(System.in);
+        n=r.nextInt();
+        while (n>0)
+        {
+            n=n/10;
+            count++;
+        }
+        System.out.println("No.of Digits" + count);
+        }
+    }
+    
+
